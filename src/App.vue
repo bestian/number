b9f723ad078306b238d69d99953586270f55ad0c
Bestian Tang <bestian@gmail.com>
2022-07-23 18:47:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav" class="ui menu">
-      <router-link class = "item" to ="/">首頁</router-link>
-      <router-link class = "item" to ="/about">加總</router-link>
+      <router-link class = "item" to ="/">
+        <i class ="home icon"/>首頁</router-link>
+      <router-link class = "item" to ="/about"><i class ="plus icon"/>加總</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,12 +16,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 18px;
 }
 
 .avatar {
   width: 60px;
   height: 60px;
   border-radius: 50px;
+}
+
+label, input {
+  font-size: 18px !important;
 }
 
 .router-link-exact-active {
